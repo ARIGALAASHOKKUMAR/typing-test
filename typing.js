03e459af0e8687af022d8e503f04e3656f257559
ARIGALA ASHOK KUMAR <95636314+ARIGALAASHOKKUMAR@users.noreply.github.com>
2022-11-27 23:50:29 +0530

    Update typing.js

--- typing.js
+++ typing.js
@@ -1,52 +1,36 @@
-let timer = document.getElementById("stopwatch")
-
-let data = document.getElementById("quoteDisplay")
-
-let user_input = document.getElementById("quoteInput")
-
-let result = document.getElementById("result")
-
-let sub_btn = document.getElementById("submitBtn")
-
-let reset_btn = document.getElementById("resetBtn")
-
-let total = document.getElementById("speedTypingTest")
-
-let spinner = document.getElementById("spinner")
-
-// let out_id;
-// let count = 0
-// let isFocused = true;
-// if(isFocused === true) {
-//     isFocused = false
-//     user_input.addEventListener("focus",time) 
+let timer = document.getElementById("stopwatch");
+let data = document.getElementById("quoteDisplay");
+let user_input = document.getElementById("quoteInput");
+let result = document.getElementById("result");
+let sub_btn = document.getElementById("submitBtn");
+let reset_btn = document.getElementById("resetBtn");
+let total = document.getElementById("speedTypingTest");
+let spinner = document.getElementById("spinner");
 let count=0 
 let out_id;
 function time() {
     let id = setInterval(function(event) {
-        count = count + 1
-        timer.textContent = count    
+        count = count + 1;
+        timer.textContent = count ;   
     },1000)
-    out_id = id
+    out_id = id;
 }
 time()
-
 sub_btn.onclick = function(event) {
     if (data.textContent == user_input.value) {
         clearInterval(out_id)        
-        result.textContent = "You typed in " + count + " seconds"
+        result.textContent = "You typed in " + count + " seconds";
     } else {
-        result.textContent = "You typed incorrect sentence"
+        result.textContent = "You typed incorrect sentence";
     }
 }
-
 reset_btn.onclick = function(event) {    
-    result.textContent=""
-    timer.textContent=0
-    total.classList.add("d-none")
-    spinner.classList.remove("d-none")
-    user_input.value = ""
-    count=0 
+    result.textContent="";
+    timer.textContent=0;
+    total.classList.add("d-none");
+    spinner.classList.remove("d-none");
+    user_input.value = "";
+    count=0; 
       
     let type = {
         method: "GET"
